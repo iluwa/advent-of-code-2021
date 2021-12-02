26ad6d59ba26bb63faf3ba84a99e49239fafd024
iluwa <noobikflash@yandex.ru>
2021-12-02 13:55:26 +0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .mapSlidingWindow(1) { a, b -> if (a.sum() < b.sum()) 1 else 0 }
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .mapSlidingWindow(3) { a, b -> if (a.sum() < b.sum()) 1 else 0 }
+            .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun <T, U> List<T>.mapSlidingWindow(windowSize: Int, f: (List<T>, List<T>) -> U): List<U> {
+    val res = mutableListOf<U>()
+    for (i in 0 until this.size - windowSize) {
+        res.add(f(this.subList(i, i + windowSize), this.subList(i + 1, i + 1 + windowSize)))
+    }
+    return res
 }
